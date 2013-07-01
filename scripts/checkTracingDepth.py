641ec1d7bb8211c52384e3bcb63939dc91ea353d
Fredrik Averpil <fredrik.averpil@gmail.com>
2013-07-01 13:25:03 +0200

    Update checkTracingDepth.py

--- scripts/checkTracingDepth.py
+++ scripts/checkTracingDepth.py
@@ -4,7 +4,7 @@ def checkTracingDepth():
 	shaders = cmds.ls(type='VRayMtl')
 	for shader in shaders:
 		if cmds.getAttr(shader+'.refractionsMaxDepth') > 5:
-			print 'refr: ' + shader
+			print 'Refractions trace depth ' + str(cmds.getAttr(shader+'.refractionsMaxDepth')) + ' :' + shader
 		if cmds.getAttr(shader+'.reflectionsMaxDepth') > 5:
-			print 'refl: ' + shader
+			print 'Reflections trace depth ' + str(cmds.getAttr(shader+'.reflectionsMaxDepth')) +  ' :' + shader
 checkTracingDepth()
